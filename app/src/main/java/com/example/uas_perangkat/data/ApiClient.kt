fdefa4be5a15be9d56eef0699de79928d0621bda
vanessa <vanessamagung@gmail.com>
2025-12-08 15:58:25 +0700

    comit oleh vanessa menambah kelas ApiClient.kt

--- app/src/main/java/com/example/uas_perangkat/data/ApiClient.kt
+++ app/src/main/java/com/example/uas_perangkat/data/ApiClient.kt
@@ -1,2 +1,24 @@
 package com.example.uas_perangkat.data
 
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private const val BASE_URL = "http://104.248.153.158/event-api/"
+
+    private val client = OkHttpClient.Builder()
+        .protocols(listOf(Protocol.HTTP_1_1))   // FIX PROTOCOL_ERROR
+        .build()
+
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
