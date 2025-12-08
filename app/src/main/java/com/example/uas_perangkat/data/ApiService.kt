899e79d0140ecbd4cf6143d96a2cab247ff2d407
vanessa <vanessamagung@gmail.com>
2025-12-08 15:59:21 +0700

    comit oleh vanessa menambah kelas ApiService.kt

--- app/src/main/java/com/example/uas_perangkat/data/ApiService.kt
+++ app/src/main/java/com/example/uas_perangkat/data/ApiService.kt
@@ -1,2 +1,10 @@
 package com.example.uas_perangkat.data
 
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("api.php")
+    suspend fun getEvents(): Response<List<Event>>
+}
