ada20d31574f3edae6ddbf2b0c00faee8c8e06ae
vanessa <vanessamagung@gmail.com>
2025-12-08 17:10:13 +0700

    comit oleh vanessa perbaiki EventRepository.kt

---
+++ app/src/main/java/com/example/uas_perangkat/repository/EventRepository.kt
@@ -0,0 +1,17 @@
+package com.example.uas_perangkat.repository
+
+
+import com.example.uas_perangkat.data.ApiClient
+import com.example.uas_perangkat.data.Event
+
+class EventRepository {
+
+    suspend fun getAllEvents(): List<Event>? {
+        val response = ApiClient.apiService.getEvents()
+        return if (response.isSuccessful) {
+            response.body()
+        } else {
+            null
+        }
+    }
+}
