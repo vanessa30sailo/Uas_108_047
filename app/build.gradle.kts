22c56f7a43e4e549fb78bbd2514faf64bbd9294d
santi <santiefendi280@gmail.com>
2025-12-09 15:38:14 +0700

    Commit oleh santy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,16 +21,22 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
@@ -38,21 +44,31 @@ android {
 
 dependencies {
 
+    // Android
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.activity:activity-compose:1.12.0")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2024.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
+
+    // Retrofit + OkHttp (BENAR)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Debug tools
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/example/uas_perangkat/repository/EventRepository.kt
+++ app/src/main/java/com/example/uas_perangkat/repository/EventRepository.kt
@@ -1,13 +1,14 @@
 package com.example.uas_perangkat.repository
 
-
 import com.example.uas_perangkat.data.ApiClient
 import com.example.uas_perangkat.data.Event
+import retrofit2.Response
 
 class EventRepository {
 
     suspend fun getAllEvents(): List<Event>? {
-        val response = ApiClient.apiService.getEvents()
+        val response: Response<List<Event>> = ApiClient.apiService.getEvents()
+
         return if (response.isSuccessful) {
             response.body()
         } else {
